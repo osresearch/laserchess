dcb1b2c78df723e329443671f0ee8623d9173ac1
Trammell hudson <hudson@trmm.net>
2018-07-29 15:49:42 -0400

    laser fire starts to work, then dies in Fire()

--- sketch.js
+++ sketch.js
@@ -575,6 +575,158 @@ function ExpLode()
 	}
 }
 
+/*
+Hit:
+term(i) = 1
+drk(i) = tdir
+IF d THEN EndBeam
+
+tx = px
+ty = py
+px = Lx(i)
+py = Ly(i)
+if piece(px,py) = 4 THEN k = k + cLr(px,py)
+if piece(px,py) = 2 THEN L(cLr(px,py)) = 0
+x = px * 27 + 16
+y = py * 19 - 6
+m = piece(px,py)
+shpt(0) = shpt(m)
+FOR j=0 to shpt(0)+1
+	shptx(0,j) = shptx(m,j)
+	shpty(0,j) = shpty(m,j)
+NEXT
+t = i
+i = 0
+co = 8
+OR orient(px,py) + 1 GOSUB rotate0, rotate90, rotate180, rotate270
+i = t
+px = tx
+py = ty
+// fall through the EndBeam
+*/
+function Hit()
+{
+	term[i] = 1;
+	drk[i] = tdir;
+	if (d)
+		return false; // EndBeam
+
+	tx = px;
+	ty = py;
+	px = Lx[i];
+	py = Ly[i];
+	if (piece[px][py] == 4)
+		k = k + cLr[px][py];
+	if (piece[px][py] == 2)
+		L[cLr[px][py]] = 0
+	x = px * 27 + 16;
+	y = py * 19 - 6;
+	m = piece[px][py];
+	shpt[0] = shpt[m];
+	for(j=0 ; j <= shpt[0]+1 ; j++)
+	{
+		shptx[0][j] = shptx[m][j];
+		shpty[0][j] = shpty[m][j];
+	}
+	t = i;
+	i = 0;
+	co = 8;
+
+	draw_shape(co);
+
+	i = t;
+	px = tx;
+	py = py;
+
+	return false; // EndBeam
+}
+
+
+/*
+bmd0: LINE(0,0)-(0,18)
+bmd1: LINE(0,0)-(26,0)
+
+DrawBeam:
+x = Lx[i] * 27 + 29
+y = Ly[i] * 19 + 3
+ON dir[i] GOTO BRt, Bdn, BLt // fall through to BRup
+BUp: PUT(x,y-19), bmd0: GOTO CkBeam
+BRt: PUT(x,y+1), bmd1: GOTO CkBeam
+BDn: PUT(x+1,y) bmd0: GOTO CkBeam
+BLt: PUT(X-27,y), bmd1: GOTO CkBeam
+
+CkBeam:
+IF (nLx(i) > 9) OR (nLy(i) > 9) OR (nLx(i) < 0) OR (nLy(i) < 0) THEN EndBeam
+IF (nLx(i) = 5 AND nLy(i) = 5) THEN EndBeam
+Lx(i) = nLx(i)
+Ly(i) = nLy(i)
+IF piece(nLx(i), nLy(i))=0 THEN Advbeam
+tdir = dir(i)
+dir(i) = dirck(piece(Lx(i),Ly(i)),orient(Lx(i),Ly(i)),dir(i))
+IF dir(i)=-1 THEN Hit
+IF dir(i)>=-2 THEN AdvBeam
+IF aLive(2)=0 THEN
+	j=2
+ELSE
+	j=3
+aLive(j)=1
+Lx(j) = Lx(i)
+Ly(j) = Ly(i)
+dir(i) = tdir+1 AND 3
+dir(j) = tdir - 1 AND 3
+GOTO AdvBeam
+*/
+function DrawBeam()
+{
+	x = Lx[i] * 27 + 29;
+	y = Ly[i] * 19 + 3;
+
+	noFill();
+	stroke(palette(10));
+
+	console.log("dir[i]="+dir[i]);
+	if (dir[i] == 1) // BRt, bmd1
+		line(x,y, x+26, y);
+	else
+	if (dir[i] == 2) // BDn, bmd0
+		line(x,y, x, y+18);
+	else
+	if (dir[i] == 3) // BLt, bmd1
+		line(x, y, x-26, y);
+	else // dir[i] == 0 BUp, bmd0
+		line(x,y, x, y-18);
+
+	// CkBeam:
+	if (nLx[i] > 9 || nLy[i] > 9 || nLx[i] < 0 || nLy[i] < 0)
+		return false; // EndBeam
+	if (nLx[i] == 5 && nLy[i] == 5)
+		return false; // EndBeam
+	Lx[i] = nLx[i];
+	Ly[i] = nLy[i];
+	if (piece[nLx[i]][nLy[i]] == 0)
+		return true; // AdvBeam
+
+	// Hit something, check for either a beam split or turn
+	tdir = dir[i];
+	dir[i] = dirck[piece[Lx[i]][Ly[i]]][orient[Lx[i]][Ly[i]]][dir[i]];
+	if (dir[i] == -1)
+		return Hit(); // always falls through to EndBeam
+	if (dir[i] >= -2)
+		return true; // AdvBeam
+
+	// Create a new laser
+	if (aLive[2] == 0)
+		j = 2;
+	else
+		j = 3;
+	aLive[j] = 1;
+	Lx[j] = Lx[i];
+	Ly[j] = Ly[i];
+	dir[i] = (tdir + 1) & 3;
+	dir[j] = (tdir - 1) & 3;
+	return true; // AdvBeam
+}
+			
 
 /*
 Fire:
@@ -594,32 +746,7 @@ WHILE(aLive(1) = 1 OR aLive(2) = 1 or aLive(3) = 1)
 		beamck(dir(i),Lx(i),Ly(i)) = 1
 		GOTO DrawBeam
 
-		Hit:
-		term(i) = 1
-		drk(i) = tdir
-		IF d THEN EndBeam
-
-		tx = px
-		ty = py
-		px = Lx(i)
-		py = Ly(i)
-		if piece(px,py) = 4 THEN k = k + cLr(px,py)
-		if piece(px,py) = 2 THEN L(cLr(px,py)) = 0
-		x = px * 27 + 16
-		y = py * 19 - 6
-		m = piece(px,py)
-		shpt(0) = shpt(m)
-		FOR j=0 to shpt(0)+1
-			shptx(0,j) = shptx(m,j)
-			shpty(0,j) = shpty(m,j)
-		NEXT
-		t = i
-		i = 0
-		co = 8
-		OR orient(px,py) + 1 GOSUB rotate0, rotate90, rotate180, rotate270
-		i = t
-		px = tx
-		py = ty
+		Hit: // see code above; falls through to end beam?
 
 		EndBeam:
 		aLive(i) = -1
@@ -627,9 +754,10 @@ WHILE(aLive(1) = 1 OR aLive(2) = 1 or aLive(3) = 1)
 	NEXT
 WEND
 */
-			
+
 function Fire()
 {
+	console.log("Fire! " + Lpx[pL] + "," + Lpy[pL]);
 	px = Lpx[pL];
 	py = Lpy[pL];
 	Lx[1] = px;
@@ -662,41 +790,64 @@ function Fire()
 
 			beamck[dir[i]][Lx[i]][Ly[i]] = 1;
 
-			return DrawBeam();
-
-// Hit:
-			term[i] = 1;
-			drk[i] = tdir;
-			if (d)
+			if (! DrawBeam())
 			{
 				aLive[i] = -1;
 				continue;
 			}
 
-			tx = px;
-			ty = py;
-			px = Lx[i];
-			py = Ly[i];
-			if (piece[px][py] == 4)
-				k = k + cLr[px][py];
-			if (piece[px][py] == 2)
-				L[cLr[px][py]] = 0
-			x = px * 27 + 16;
-			y = py * 19 - 6;
-			m = piece[px][py];
-			shpt[0] = shpt[m];
-			for(j=0 ; j <= shpt[0]+1 ; j++)
-			{
-				shptx[0][j] = shptx[m][j];
-				shpty[0][j] = shpty[m][j];
-			}
-			t = i;
-			i = 0;
-			co = 8;
-			draw_shape(co);
 		}
 	}
 }
+
+/*
+Laser:
+k=0
+d=0
+GOSUB Fire
+FOR i=0 to 3
+	FOR x=1 to 9
+		FOR y=1 to 9
+			beamck(i,x,y) = 0
+NEXT y,x,i
+FOR i=1 to 3
+	IF term(i) THEN
+		IF piece(Lx(i),Ly(i)) > 0 THEN
+			tx = px
+			ty = py
+			px = Lx(i)
+			py = Ly(i)
+			GOSUB ExpLode
+			px = tx
+			py = ty
+		END IF
+	END IF
+NEXT
+TIMER OFF
+d = 1
+GOSUB Fire
+FOR i=1 to 3
+	IF term(i) THEN
+		IF piece(Lx(i),Ly(i)) > 0 THEN
+			tx = px
+			ty = py
+			px = Lx(i)
+			py = Ly(i)
+			piece(px,py) = 0
+			cLr(px,py) = 0
+			GOSUB EraseSquare
+			px = tx
+			py = ty
+		END IF
+	END IF
+NEXT
+FOR i=0 to 3
+	FOR x=1 to 9
+		FOR y=1 to 9
+			beamck(i,x,y) = 0
+NEXT y,x,i
+RETURN
+*/
 
 function Laser()
 {
@@ -704,29 +855,21 @@ function Laser()
 	k = 0;
 	d = 0;
 
-	for(i=1 ; i <= 3 ; i++)
+	// create the beamck array
+	for(i=0 ; i <= 3 ; i++)
 	{
 		beamck[i] = [];
 		for(x=1 ; x <= 9 ; x++)
-		{
 			beamck[i][x] = [];
-			for(y=1 ; y <= 9 ; y++)
-				beamck[i][x][y] = 0
-		}
 	}
+				
 
 	Fire();
 
 	for(i=1 ; i <= 3 ; i++)
-	{
-		beamck[i] = [];
 		for(x=1 ; x <= 9 ; x++)
-		{
-			beamck[i][x] = [];
 			for(y=1 ; y <= 9 ; y++)
 				beamck[i][x][y] = 0
-		}
-	}
 
 	for(i=1 ; i <= 3 ; i++)
 	{
@@ -747,7 +890,7 @@ function Laser()
 	d = 1;
 	Fire();
 
-	for(i=1 ; i <= 3 ; i++)
+	for(i=0 ; i <= 3 ; i++)
 		for(x=1 ; x <= 9 ; x++)
 			for(y=1 ; y <= 9 ; y++)
 				beamck[i][x][y] = 0;
